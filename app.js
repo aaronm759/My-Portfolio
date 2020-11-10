ef73928dc3c153e7cce8d254226f01b087446760
aaronm759 <63614423+aaronm759@users.noreply.github.com>
2020-11-10 09:45:53 -0500

    live version

---
+++ app.js
@@ -0,0 +1,8 @@
+function popUp() {
+    document.getElementById('popup').style.display = 'block';
+}
+function close() {
+    document.getElementById('popup').style.display = 'none';
+}
+document.getElementById('contact').addEventListener("click", popUp);
+document.getElementById('popup').addEventListener("click", close)
